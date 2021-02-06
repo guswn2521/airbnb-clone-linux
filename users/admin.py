d385465e9e9db760dc835d0aaa50bd0cf7ca2b3b
guswn2521 <guswn2521@naver.com>
2021-02-06 20:39:58 +0900

    #3.5 UserAdmin + CustomAdmin 1. from django.contrib.auth.admin import UserAdmin class CustomUserAdmin(UserAdmin) : 기본 user info 가져오기. 2. fieldset : 기본 + custom user info fieldset추가

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 # Register your models here.
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
 
     """ Custom User Admin """
 
-    list_display = ("username", "gender", "language", "currency", "email", "superhost")
-    list_filter = ("superhost", "currency", "language")
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                ),
+            },
+        ),
+    )
