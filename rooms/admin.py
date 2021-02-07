f687ee055558b01a9525c41bbbf89a421b2c46f6
guswn2521 <guswn2521@naver.com>
2021-02-07 19:42:07 +0900

    #4.5 Meta class and photos model 1. class Meta : verbose_name_plural : 안 주면 model 이름을 사용함. verbose_name : Model이름 대신 사용할 값.
    2. model을 가져올 때 문자열로 가져올 수 있음. => 아래에 정의된 class도 위에서 사용가능.

--- rooms/admin.py
+++ rooms/admin.py
@@ -4,11 +4,24 @@ from . import models
 
 @admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
+
+    """ Item Admin Definition """
+
     pass
 
 
 # Register your models here.
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+
+    """ Room Admin Definition """
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    """ Photo Admin Definition """
 
     pass
