1b69998aac1ff4cc5191d99b262080966896c445
guswn2521 <guswn2521@naver.com>
2021-02-08 00:28:41 +0900

    #5.1 Reservations Model 1. reservations/models 구축

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.Reservations)
+class ReservationsAdmin(admin.ModelAdmin):
+
+    """ Reservations Admin Definition """
+
+    pass
