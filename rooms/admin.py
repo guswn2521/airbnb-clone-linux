28f12c99abcdbbc44887005f820ccfa922c494ee
guswn2521 <guswn2521@naver.com>
2021-02-10 00:58:39 +0900

    #6.1, #6.2 admin panel 꾸미기 admin 설정. search_fields : 검색기능 filter_horizontal : many to many 관계에서 동작. 다 대 다로 선택,삭제,검색가능. list_display : admin 패널에서 보이는 항목들 list_filter : 정렬방식으로 설정할 항목들 fieldsets : 섹션나누고 제목정하기. classes: (collapse,) : 접기기능

--- rooms/admin.py
+++ rooms/admin.py
@@ -16,7 +16,73 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {
+                "fields": ("name", "description", "country", "address", "price"),
+            },
+        ),
+        (
+            "Times",
+            {
+                "fields": ("check_in", "check_out", "instant_book"),
+            },
+        ),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        (
+            "spaces",
+            {
+                "fields": ("guests", "beds", "bedrooms", "baths"),
+            },
+        ),
+        (
+            "Last Details",
+            {
+                "fields": ("host",),
+            },
+        ),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "host__gender",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = ("^city", "^host__username")
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
 
 
 @admin.register(models.Photo)
