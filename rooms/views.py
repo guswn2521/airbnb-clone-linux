bdcd83abd3fafb476c56c67248be76be6c1e6de3
guswn2521 <guswn2521@naver.com>
2021-02-20 00:34:15 +0900

    # 11.2 Pages List Navigation 1. page_count : objects.count() / page_size 2. page_range : 장고에서 range안됨. views.py에서 range 적용한 값을 변수로 넘겨줘야함.

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,4 @@
+from math import ceil
 from django.shortcuts import render
 from . import models
 
@@ -9,5 +10,15 @@ def all_rooms(request):
     page_size = 10
     limit = page_size * page
     offset = limit - page_size
+    page_count = ceil(models.Room.objects.count() / page_size)
     all_rooms = models.Room.objects.all()[offset:limit]
-    return render(request, "rooms/home.html", context={"rooms": all_rooms})
+    return render(
+        request,
+        "rooms/home.html",
+        context={
+            "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count + 1),
+        },
+    )
