d68b42b222a89229fd07d8155a9a723f156d75fc
guswn2521 <guswn2521@naver.com>
2021-02-20 20:05:47 +0900

    # 11.7 Class Based Views (pagination 3번째 방법.) : 변수명이나 파일명이 지정되어 있음. 1. List View : class based view라는 family에서 옴. objects의 목록을 보여주는 페이지. from django.views.generic import ListView 1) class HomeView생성. : ListView 상속. 2) model 생성. etc) paginate_by : 페이징 할 아이템수.
    2. core/urls.py  - path 변경
    urlpatterns의 path는 오로지 url, 함수만 가짐. class를 넣으면 error 발생.
    => class.as_view() : 클래스를 view로 변경해주는 메소드.
    
    3. templates/rooms_list.html
    1) template 파일 이름 -> 모델명_list로
    2) object_list : 전체 page_obj의 리스트. paginator에서 page_obj.object_list와 같음. view 가 작동하는 objects의 리스트가 든 변수
    3) page_obj : 한 페이지에 대한 paginator.Page 객체.
    
    *장고 class*
    https://ccbv.co.uk/projects/Django/3.0/django.views.generic.list/ListView/

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from rooms import views as rooms_views
 app_name = "core"
 
 urlpatterns = [
-    path("", rooms_views.all_rooms, name="home"),
+    path("", rooms_views.HomeView.as_view(), name="home"),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,21 +1,14 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.views.generic import ListView
 from . import models
 
 # Create your views here.
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=6)
-    try:
-        rooms = paginator.page(int(page))
-        return render(
-            request,
-            "rooms/home.html",
-            context={"page": rooms},
-        )
-    except EmptyPage:
-        return redirect("/")
-    # print(rooms.object_list)
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 6
+    ordering = "price"
