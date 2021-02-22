eee96ac95496cfe1f744e4a4c62a2999e5fa9e92
guswn2521 <guswn2521@naver.com>
2021-02-22 22:37:07 +0900

    #12.2 room_detail FBV finished 1. rooms/views.py room_detail( request, pk ) 함수 - request 받은 room objects를 가져와서 context로 전달.
    2. rooms/detail.html
    content 블록에 room.name, room.description, room.host.username, room.host.superhost, room.amenities.all, room.facilities.all, room.house_rules.all 가져옴.

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.urls import reverse
 from django.shortcuts import render, redirect
 from . import models
 
@@ -17,4 +18,9 @@ class HomeView(ListView):
 
 
 def room_detail(request, pk):
-    return render(request, "rooms/detail.html")
+
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        return redirect(reverse("core:home"))
