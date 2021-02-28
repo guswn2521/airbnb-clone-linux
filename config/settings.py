5d46c2e6e6c9e3ceb9c60b77cf9e95c435b33737
guswn2521 <guswn2521@naver.com>
2021-02-28 15:28:57 +0900

    # 16.2 Verifying Email part one 1. models.py verify_email method : 시크릿 키를 생성해서 메일 보내는 함수. 1) import uuid => secret = uuid.uuid4().hex[:20] 랜덤 20자리 생성. 2) from django.conf import settings 3) from django.core.mail import send_mail 4) send_mail(subjects, message, from_mail, recipient_list, fail_silently) - subjects : 메일제목 - from_mail : settings.py에서 EMAIL_FROM가져옴.

--- config/settings.py
+++ config/settings.py
@@ -145,3 +145,4 @@ EMAIL_HOST = "smtp.mailgun.org"
 EMAIL_PORT = "587"
 EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
 EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
+EMAIL_FROM = "guswn1@sandbox1be9a20bc7704c49bd987746f5a49568.mailgun.org"
