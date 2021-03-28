c5878e48283d6a5aa81ea59b980b5b7a0bbf9163
guswn2521 <guswn2521@naver.com>
2021-03-28 18:58:52 +0900

    #  21.1 Messages part Two 1. views.py 에 Kakao Exception, Github Exception 메세지 추가. 2. messages.html 스타일링.

--- users/views.py
+++ users/views.py
@@ -98,7 +98,7 @@ def github_callback(request):
             result_json = result.json()
             error = result_json.get("error", None)
             if error is not None:
-                raise GithubException()
+                raise GithubException("Can't get access token")
 
             # error 가 없으면
             else:
@@ -120,8 +120,10 @@ def github_callback(request):
                     bio = "" if bio is None else bio
                     try:
                         user = models.User.objects.get(email=email)
-                        if user.login_method == models.User.LOGIN_GH:
-                            raise GithubException()
+                        if user.login_method != models.User.LOGIN_GH:
+                            raise GithubException(
+                                f"Please log in with : {user.login_method}"
+                            )
                     except models.User.DoesNotExist:
                         user = models.User.objects.create(
                             email=email,
@@ -134,13 +136,15 @@ def github_callback(request):
                         user.set_unusable_password()
                         user.save()
                     login(request, user)
+                    messages.success(request, f"Welcome back {user.first_name}")
                     return redirect(reverse("core:home"))
                 else:
-                    raise GithubException()
+                    raise GithubException("Can't get your profile")
 
         else:
-            raise GithubException()
-    except GithubException:
+            raise GithubException("Can't get code.")
+    except GithubException as e:
+        messages.error(request, e)
         return redirect(reverse("users:login"))
 
 
@@ -159,7 +163,7 @@ class KakaoException(Exception):
 def kakao_callback(request):
     try:
         code = request.GET.get("code")
-        raise KakaoException()
+        # raise KakaoException("Something went wrong.")
         client_id = os.environ.get("KAKAO_ID")
         redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
         token_request = requests.get(
@@ -168,7 +172,7 @@ def kakao_callback(request):
         token_json = token_request.json()
         error = token_json.get("error", None)
         if error is not None:
-            raise KakaoException()
+            raise KakaoException("Can't get authorization code.")
         access_token = token_json.get("access_token")
         profile_request = requests.get(
             "https://kapi.kakao.com/v2/user/me",
@@ -177,14 +181,14 @@ def kakao_callback(request):
         profile_json = profile_request.json()
         email = profile_json.get("kakao_account").get("email")
         if email is None:
-            raise KakaoException()
+            raise KakaoException("Please also give me your email")
         properties = profile_json.get("properties")
         nickname = properties.get("nickname")
         profile_image = properties.get("profile_image")
         try:
             user = models.User.objects.get(email=email)
             if user.login_method != models.User.LOGIN_KAKAO:
-                raise KakaoException()
+                raise KakaoException(f"Please log in with : {user.login_method}")
         except models.User.DoesNotExist:
             user = models.User.objects.create(
                 email=email,
@@ -201,8 +205,9 @@ def kakao_callback(request):
                     f"{nickname}-avatar.jpg", ContentFile(photo_request.content)
                 )
         login(request, user)
+        messages.success(request, f"Welcome back {user.first_name}")
         return redirect(reverse("core:home"))
 
-    except KakaoException:
-        messages.error(request, "Something went wrong.")
+    except KakaoException as e:
+        messages.error(request, e)
         return redirect(reverse("users:login"))
