12b3094a948104d5f7ebb66348a10069b506c80a
guswn2521 <guswn2521@naver.com>
2021-04-13 00:55:44 +0900

    # 23.4 Edit Photo 1. EditPhotoView 생성. 1) user_mixins.LoggedInOnlyView, SuccessMessageMixin 상속 2) get_success_url함수 생성 : success_url변수에 안주고 함수생성한 이유는 room_pk 인자를 전달하기 위해서 url에서 pk받아오는법 : self.kwargs.get("pk 변수명") 3) pk_url_kwarg : UpdateView는 pk가 default 변수명임. -> pk 명을 바꿀때. 2. photo_edit.html생성 3. url연결 4. room_photos.html 버튼 링크

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,13 +4,18 @@ from . import views
 app_name = "rooms"
 
 urlpatterns = [
-    path("<int:pk>", views.RoomDetail.as_view(), name="detail"),
+    path("<int:pk>/", views.RoomDetail.as_view(), name="detail"),
     path("<int:pk>/edit/", views.RoomUpdateView.as_view(), name="edit"),
-    path("<int:pk>/photos", views.RoomPhotosView.as_view(), name="photos"),
+    path("<int:pk>/photos/", views.RoomPhotosView.as_view(), name="photos"),
     path(
         "<int:room_pk>/photos/<int:photo_pk>/delete/",
         views.delete_photos,
         name="delete-photo",
+    ),
+    path(
+        "<int:room_pk>/photos/<int:photo_pk>/edit/",
+        views.EditPhotoView.as_view(),
+        name="edit-photo",
     ),
     path("search/", views.SearchView.as_view(), name="search"),
 ]
