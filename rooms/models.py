145c270f0529c763c268109950717e3dc9565730
guswn2521 <guswn2521@naver.com>
2021-04-17 16:32:15 +0900

    #23.8 Creating Room part One host일 때 방을 생성하는 view생성 1. views.py : CreateRoomView(LoggedInOnly, FormView)생성 2. forms.py : CreateRoomForm(forms.Mdelform)생성 1) class Meta에 model, field줌 2) save함수 override : 3. url연결 4. models.py - first_photo에 try/except추가. 5. room_create.html생성 6. nav.html - host일 때 Create Room 버튼, *새로 생성한 방의 rooms:detail로 redirect하기 위해 새로운 방의 pk필요 => form에서 방을 commit=False로 저장하고 return => view form_valid에서 form.save로 room받아와서 host 넣고 room.save() => 새로운 방 room detail로 return redirect

--- rooms/models.py
+++ rooms/models.py
@@ -114,8 +114,11 @@ class Room(core_models.TimeStampedModel):
             return round(all_ratings / len(all_reviews), 2)
 
     def first_photo(self):
-        (photo,) = self.photos.all()[:1]
-        return photo.file.url
+        try:
+            (photo,) = self.photos.all()[:1]
+            return photo.file.url
+        except:
+            return None
 
     def get_next_four_photos(self):
         photos = self.photos.all()[1:5]
