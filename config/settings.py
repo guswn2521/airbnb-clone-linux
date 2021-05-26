10345d6921be0ea6b87bedc8af90f1a4913c30d7
guswn2521 <guswn2521@naver.com>
2021-05-26 15:21:44 +0900

    .env에 DEBUG=True 추가 settings.py에 if else문으로 DEBUG가 True이면 sqlite3를 사용하고, DEBUG가 False이면 postgresql을 사용하도록 함. **.env는 .gitignore에 있어서 elastic beanstalk에 업로드되지않음. ** requirements.txt의 마지막줄은 빈칸이어야함.

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "hldf2b&mv%i4zf2ps0&*3u9^kf(56jz=7ptv6-0r9flytn1ay&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = ["*"]
 
@@ -89,13 +89,21 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+if DEBUG:
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 
 
 # Password validation
